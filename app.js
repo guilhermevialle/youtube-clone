e8e0c49ed0e379c4200ea44331f68f787aae45a2
guilhermevialle <guilhef2@hotmail.com>
2021-03-04 02:00:39 -0300

    animacoes sidebar

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log(window.innerWidth, window.innerHeight);
+const menu = document.querySelectorAll(".logoAndMenu svg")[0];
+const leftBar = document.querySelector(".leftBar");
+const videos = document.querySelectorAll(".video");
+const videoSection = document.querySelector(".videoSection");
+const leftBarButtons = document.querySelectorAll(".leftBar button");
+const svgInsideButtons = document.querySelectorAll(".leftBar button svg");
+const buttonTexts = document.querySelectorAll(".leftBar button p");
+
+// animating youtube side bar
+var count = 0;
+
+const changeWidth = () => {
+    if (count == 1) {
+        leftBar.style.width = "5.3vw";
+        leftBar.style.backgroundColor = "#252525";
+        leftBarButtons.forEach((btn, index) => {
+            btn.style.height = "76px";
+            btn.style.flexDirection = "column";
+            btn.style.justifyContent = "center";
+            svgInsideButtons[index].style.margin = "0";
+            buttonTexts[index].style.fontSize = "10px";
+        });
+        // videos.forEach((video) => {
+        // video.style.width = "292px";
+        // video.style.height = "315px";
+        // video.style.transform = "scale(1, 1)";
+        // });
+        videoSection.style.width = "94vw";
+        count = 0;
+        return;
+    }
+
+    count = 1;
+    leftBar.style.width = "17.6vw";
+    leftBar.style.backgroundColor = "#202020";
+    leftBarButtons.forEach((btn, index) => {
+        btn.style.height = "50px";
+        btn.style.flexDirection = "row";
+        btn.style.justifyContent = "start";
+        svgInsideButtons[index].style.margin = "40px";
+        buttonTexts[index].style.fontSize = "1rem";
+    });
+    // videos.forEach((video) => {
+    // video.style.width = "250px";
+    // video.style.transform = "scale(0.9, 0.9)";
+    // });
+    videoSection.style.width = "82.4vw";
+};
+
+menu.addEventListener("click", changeWidth, false);
